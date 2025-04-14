549fc401de84fd7c3838e65b139c244d2c89531d
lolaescalass <lolaescalaspralong@gmail.com>
2025-04-14 20:08:42 -0300

    Add files via upload
    Solo falta funcion ordenar, toda la parte de INodo ILista etc, y hacer el test.

---
+++ Vehiculo.java
@@ -0,0 +1,31 @@
+package modelo;
+
+public class Vehiculo {
+
+	private String patente;
+	private String modelo;
+	
+	public String getPatente() {
+		return patente;
+	}
+	public void setPatente(String patente) {
+		this.patente = patente;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	@Override
+	public String toString() {
+		return "Vehiculo [patente=" + patente + ", modelo=" + modelo + "]";
+	}
+	public Vehiculo(String patente, String modelo) {
+		super();
+		this.patente = patente;
+		this.modelo = modelo;
+	}
+	
+	
+}
