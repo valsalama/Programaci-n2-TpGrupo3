45a7471a295e049b37e352f1f7fc8ad5731be751
lolaescalass <lolaescalaspralong@gmail.com>
2025-04-25 10:52:08 -0300

    Update ILista.java

--- src/Interfaces/ILista.java
+++ src/Interfaces/ILista.java
@@ -1,29 +1,37 @@
-package Interfaces;
+package interfaz;
 
-import Modelos.Vehiculo;
 
 public interface ILista {
 	
-    // Primitivas de la lista, métodos elementales de la estrucutra
-    // Para insertar
-    public void insertarPrimero(Vehiculo a);
-    public void insertarUltimo(Vehiculo a); // Método de cola o pila
-    public void insertarGenerico(Vehiculo a, int pos);
-	
-    // Para eliminar
-    public Vehiculo eliminarPrimero(); // Método de cola
-    public Vehiculo eliminarUltimo(); // Método de pila
-    public Vehiculo eliminarGenerico(int pos);
-	
-    // Para obtener
-    public Vehiculo obtenerPrimero(); // Método de cola
-    public Vehiculo obtenerUltimo(); // Método de pila
-    public Vehiculo obtenerGenerico(int pos);
-	
-    // Métodos extras
-    public boolean esVacia();
-    public int cantidadElementos();
-    public void ordenar();
-    public void mostrar();
-    public int buscar(Vehiculo a);
+	//Métodos
+		public INodo getPrimero();
+		public void setPrimero(INodo primero);
+		@Override
+		public String toString();
+		
+		///Primitivas de la lista, métodos elementales de la estrucutra
+		
+		//Para insertar
+		public void insertarPrimero(IVehiculo a);
+		public void insertarUltimo(IVehiculo a); //metodo de cola o pila
+		public void insertarGenerico(IVehiculo a, int pos);
+		
+		//Para eliminar
+		//puede retornar el nodo o el dato eliminado, como queira 
+		public IVehiculo eliminarPrimero(); //metodo de cola
+		public IVehiculo eliminarUltimo(); //metodo de pila
+		public IVehiculo eliminarGenerico(int pos);
+		
+		//Para obtener
+		public IVehiculo obtenerPrimero(); //metood de cola
+		public int obtenerUltimo(); //metodo de pila
+		public IVehiculo obtenerGenerico(int pos);
+		
+		//Métodos extras
+		public boolean esVacia();
+		public int cantidadElementos();
+		public void ordenar();
+		public void mostrar();
+		public int buscar(IVehiculo a);
 }
+
