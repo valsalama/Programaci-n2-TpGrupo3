e008d758c0a1a57f42e056207f183bbabf8f7b2d
lolaescalass <lolaescalaspralong@gmail.com>
2025-04-25 10:53:41 -0300

    Update IVehiculo.java

--- src/Interfaces/IVehiculo.java
+++ src/Interfaces/IVehiculo.java
@@ -1,12 +1,9 @@
-package Interfaces;
+package interfaz;
 
 public interface IVehiculo {
 	
-    // Modelo
-    public String getModelo();
-    public void setModelo(String modelo);
-
-    // Patente
-    public String getPatente();
-    public void setPatente(String patente);
+	public String getPatente();
+	public void setPatente(String patente);
+	public String getModelo();
+	public void setModelo(String modelo);
 }
