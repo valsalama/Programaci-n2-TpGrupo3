4456848c17ae397e41bb4c01606e5085ade88b64
lolaescalass <lolaescalaspralong@gmail.com>
2025-04-25 10:50:55 -0300

    Update Persona.java

--- src/Modelos/Persona.java
+++ src/Modelos/Persona.java
@@ -1,55 +1,55 @@
-package Modelos;
+package modelo;
 
-import Interfaces.IPersona;
+import interfaz.ILista;
+import interfaz.IPersona;
 
 public class Persona implements IPersona {
+	
+	private int dni;
+	private String nombre;
+	//Lo más importante del TP
+	//anidado por medio de un array Vehiculo[3]
+	private ILista listaVehiculos;
+	
+	public int getDni() {
+		return dni;
+	}
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	//Prestar atención
+	public Persona(int dni, String nombre) {
+		super();
+		this.dni = dni;
+		this.nombre = nombre;
+		this.listaVehiculos = new Lista();//solo se crea la lista
+	}
 
-    private String nombre;
-    private int dni;
-    private Lista listaVehiculos;
+	@Override
+	public String toString() {
+		return "Persona [dni=" + dni + ", nombre=" + nombre +"]";
+	}
+	
+	public ILista getListaVehiculos() {
+		return listaVehiculos;
+	}
+	public void setListaVehiculos(ILista listaVehiculos) {
+		this.listaVehiculos = listaVehiculos;
+	}
+	
+	public void mostrarPersona() {
+		
+		System.out.println(this);
+		listaVehiculos.mostrar();
+		
+	}
+	
+	
 
-    // Constructor
-    public Persona(String nombre, int dni) {
-        super();
-        this.nombre = nombre;
-        this.dni = dni;
-        this.listaVehiculos = new Lista(); // Se crea la lista
-    }
-
-    // Getters y Setters
-    // Nombre
-    public String getNombre() {
-        return nombre;
-    }
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    // DNI
-    public int getDni() {
-        return dni;
-    }
-    public void setDni(int dni) {
-        this.dni = dni;
-    }
-
-    // Lista de Vehículos
-    public Lista getListaVehiculos() {
-        return listaVehiculos;
-    }
-    public void setListaVehiculos(Lista lista) {
-        this.listaVehiculos = lista;
-    }
-
-    // Mostrar persona con sus vehículos
-    public void mostrarPersona() {
-        System.out.println(this);
-        listaVehiculos.mostrar();
-    }
-
-    // toString
-    @Override
-    public String toString() {
-        return "Persona [ nombre= " + nombre + ", dni= " + dni + " ]";
-    }
 }
