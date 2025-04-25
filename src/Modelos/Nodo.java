811a255c2a748b5cc506741fed466cbf862d1a7d
lolaescalass <lolaescalaspralong@gmail.com>
2025-04-25 10:50:17 -0300

    Update Nodo.java

--- src/Modelos/Nodo.java
+++ src/Modelos/Nodo.java
@@ -1,48 +1,40 @@
-package Modelos;
+package modelo;
 
-import Interfaces.INodo;
+import interfaz.INodo;
+import interfaz.IVehiculo;
 
 public class Nodo implements INodo {
 
-    private Vehiculo dato;
-    private INodo siguiente;
+	private IVehiculo dato;
+    private INodo siguiente; //INodo
     private INodo anterior;
 
-    // Contructor
-    public Nodo(Vehiculo dato) {
+    public INodo getAnterior() {
+		return anterior;
+	}
+	public void setAnterior(INodo anterior) {
+		this.anterior = anterior;
+	}
+	public Nodo(IVehiculo dato) {
         this.dato = dato;
         this.siguiente = null;
         this.anterior = null;
     }
-
-    // Getter y Setters
-    // Datos
-    public Vehiculo getDato() {
+    public IVehiculo getDato() {
         return dato;
     }
-    public void setDato(Vehiculo dato) {
+    public void setDato(IVehiculo dato) {
         this.dato = dato;
     }
-
-    // Anterior
-    public INodo getAnterior() {
-    	return anterior;
-    }
-    public void setAnterior(INodo anterior) {
-        this.anterior = anterior;
-    }
-
-    // Siguiente
     public INodo getSiguiente() {
         return siguiente;
     }
     public void setSiguiente(INodo nodo) {
         this.siguiente = nodo;
     }
-
-    // String
-    @Override
-    public String toString() {
-        return "Nodo [dato=" + dato + ", siguiente=" + siguiente + ", anterior=" + anterior + "]";
-    }	
+	@Override
+	public String toString() {
+		return "Nodo [dato=" + dato + ", anterior=" + anterior + "]";
+	}
+	
 }
