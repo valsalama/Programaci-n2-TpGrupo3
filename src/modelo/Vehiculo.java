e7670937a70fd486f96c7812c9d57985898e0804
tecladus <126209521+tecladus@users.noreply.github.com>
2025-04-25 10:58:56 -0300

    Se cambio el nombre de las carpetas

---
+++ src/modelo/Vehiculo.java
@@ -0,0 +1,39 @@
+package modelo;
+
+import interfaz.IVehiculo;
+
+public class Vehiculo implements IVehiculo{
+
+    private String modelo;
+    private String patente;
+
+    // Constructor
+    public Vehiculo(String modelo, String patente) {
+        super();
+        this.modelo = modelo;
+        this.patente = patente;
+    }
+	
+    // Getter y Setters
+    // Modelo
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    // Patente
+    public String getPatente() {
+        return patente;
+    }
+    public void setPatente(String patente) {
+        this.patente = patente;
+    }
+
+    // String
+    @Override
+    public String toString() {
+        return "Vehiculo [ modelo= " + modelo + ", patente= " + patente + " ]";
+    }
+}
