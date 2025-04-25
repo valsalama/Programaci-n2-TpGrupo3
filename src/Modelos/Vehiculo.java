113ae59b717d4db08f04ca91495a791eddc95e0e
tecladus <126209521+tecladus@users.noreply.github.com>
2025-04-25 09:47:13 -0300

    Update Vehiculo.java

--- src/Modelos/Vehiculo.java
+++ src/Modelos/Vehiculo.java
@@ -4,36 +4,36 @@ import Interfaces.IVehiculo;
 
 public class Vehiculo implements IVehiculo{
 
-	private String modelo;
-	private String patente;
+    private String modelo;
+    private String patente;
 
     // Constructor
-	public Vehiculo(String modelo, String patente) {
-		super();
-		this.modelo = modelo;
-		this.patente = patente;
-	}
+    public Vehiculo(String modelo, String patente) {
+        super();
+        this.modelo = modelo;
+        this.patente = patente;
+    }
 	
     // Getter y Setters
     // Modelo
-	public String getModelo() {
-		return modelo;
-	}
-	public void setModelo(String modelo) {
-		this.modelo = modelo;
-	}
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
 
     // Patente
-	public String getPatente() {
-		return patente;
-	}
-	public void setPatente(String patente) {
-		this.patente = patente;
-	}
+    public String getPatente() {
+        return patente;
+    }
+    public void setPatente(String patente) {
+        this.patente = patente;
+    }
 
     // String
-	@Override
-	public String toString() {
-		return "Vehiculo [ modelo= " + modelo + ", patente= " + patente + " ]";
-	}
+    @Override
+    public String toString() {
+        return "Vehiculo [ modelo= " + modelo + ", patente= " + patente + " ]";
+    }
 }
