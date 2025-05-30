5d0f53742d1c02afe4aa7e677348a5c05ac8d6a0
lolaescalass <lolaescalaspralong@gmail.com>
2025-05-30 17:18:38 -0300

    Update IPersona.java

--- Tp2/src/interfaz/IPersona.java
+++ Tp2/src/interfaz/IPersona.java
@@ -1,10 +1,11 @@
-package interfaz;
+package Interfaz;
 
-public interface IPersona {
+public interface IPersona extends Comparable<IPersona> {
 	
 	public int getDni();
 	public void setDni(int dni);
 	public String getNombre();
 	public void setNombre(String nombre);
 	public String toString();
+	public int compareTo(IPersona p);
 }
