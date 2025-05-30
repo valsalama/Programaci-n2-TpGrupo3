0c229586a23dd9d50d45eee4415d74c18b982f38
lolaescalass <lolaescalaspralong@gmail.com>
2025-05-30 17:13:54 -0300

    Update IArbol.java

--- Tp2/src/interfaz/IArbol.java
+++ Tp2/src/interfaz/IArbol.java
@@ -1,18 +1,21 @@
-package interfaz;
+package Interfaz;
 
-import model.Persona;
-import model.Nodo;
-
-public interface IArbol {
+public interface IArbol<T extends IPersona> {
 	
-	public Nodo getRaiz();
-	public void setRaiz(Nodo raiz);
+	public INodo<T> getRaiz();
+	public void setRaiz(INodo<T> raiz);
 	public String toString();
-	public void insertar(Persona dato);
-	public void eliminar(Persona dato);
-	public Nodo buscar(Persona dato);
-	public Nodo encontrarMinimo(Nodo n);
+	public void insertar(T dato);
+	public INodo<T> insertarRecursivo(INodo<T> nodoRaiz, T dato);
+	public void eliminar(T dato);
+	public INodo<T> eliminarRecursivo(INodo<T> nodoRaiz, T dato);
+	public INodo<T> encontrarMinimo(INodo<T> n);
+	public INodo<T> buscar(T dato);
+	public INodo<T> buscarRecursivo(INodo<T> nodo, T dato);
 	public void recorridoInorden();
+	public void inordenRecursivo(INodo<T> nodo);
 	public void recorridoPreorden();
+	public void preordenRecursivo(INodo<T> nodo);
 	public void recorridoPostorden();
+	public void postordenRecursivo(INodo<T> nodo);
 }
