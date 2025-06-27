87c28b3db59667abeb9d3156747a6dddaa4cefcb
lolaescalass <lolaescalaspralong@gmail.com>
2025-06-27 02:27:23 -0300

    Update INodo.java

--- TPO/src/interfaz/INodo.java
+++ TPO/src/interfaz/INodo.java
@@ -2,28 +2,28 @@ package interfaz;
 
 public interface INodo {
 
-    String getNombre();
+    public String getNombre();
     /**
      * Pre: El nodo esta correctamente inicializado.
      * Post: Se retorna el nombre asignado al nodo.
      * Axioma: El valor devuelto corresponde al identificador unico del nodo.
      */
 
-    void setNombre(String nombre);
+    public void setNombre(String nombre);
     /**
      * Pre: El parametro nombre no es null ni vacio.
      * Post: El nombre del nodo queda actualizado con el valor recibido.
      * Axioma: getNombre() devuelve el nuevo nombre despues de la llamada.
      */
 
-    boolean equals(Object otro);
+    public boolean equals(Object otro);
     /**
      * Pre: El objeto pasado puede ser null o no ser un nodo.
      * Post: Devuelve true si el objeto es un nodo y tiene igual nombre.
      * Axioma: La comparacion es reflexiva, simetrica y transitiva.
      */
 
-    int hashCode();
+    public int hashCode();
     /**
      * Pre: El nodo esta correctamente inicializado.
      * Post: Devuelve un codigo hash consistente con equals().
